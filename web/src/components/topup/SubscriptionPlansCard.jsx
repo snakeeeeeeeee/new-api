09cda947720bdb2a9d7c8337cd7fe96cc5d748d7
snakeeeeeee <zy316161583@gmail.com>
2026-04-16 11:33:37 +0800

    订阅管理，$改为¥

--- web/src/components/topup/SubscriptionPlansCard.jsx
+++ web/src/components/topup/SubscriptionPlansCard.jsx
@@ -482,7 +482,7 @@ const SubscriptionPlansCard = ({
               {plans.map((p, index) => {
                 const plan = p?.plan;
                 const totalAmount = Number(plan?.total_amount || 0);
-                const { symbol, rate } = getCurrencyConfig();
+                const { rate } = getCurrencyConfig();
                 const price = Number(plan?.price_amount || 0);
                 const convertedPrice = price * rate;
                 const displayPrice = convertedPrice.toFixed(
@@ -560,7 +560,7 @@ const SubscriptionPlansCard = ({
                       <div className='py-2'>
                         <div className='flex items-baseline justify-start'>
                           <span className='text-xl font-bold text-purple-600'>
-                            {symbol}
+                            ¥
                           </span>
                           <span className='text-3xl font-bold text-purple-600'>
                             {displayPrice}
